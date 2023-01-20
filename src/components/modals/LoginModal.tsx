7cf3ed006ef3d4fc2109f755eb0ef89121a27c9d
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-01-20 01:36:37 +0100

    fix login auth / login toast

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -7,11 +7,11 @@ import {
   FormLabel,
   Input,
   Button,
+  useToast,
 } from "@chakra-ui/react";
 import ModalCmp from "./ModalCmp";
 import "firebase/auth";
 import * as yup from "yup";
-import { FORMVALIDATOR } from "validator/FormValidator";
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -21,6 +21,7 @@ import { useFormik } from "formik";
 import { FormikProvider } from "formik/dist/FormikContext";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { LOGINVALIDATOR } from "validator/LoginValidator";
 
 const LoginModal: React.FC<{
   isOpen: boolean;
@@ -41,12 +42,18 @@ const LoginModal: React.FC<{
   yesText,
   noText,
 }) => {
-  const [, setLoggedIn] = useState(false);
-  const [, setUserDetails] = useState(null);
+  const toast = useToast({
+    position: "top",
+    containerStyle: {
+      zIndex: 9,
+    },
+  });
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userDetails, setUserDetails] = useState(null);
   // page loading state
   /* const [pageLoading, setPageLoading] = useState(false); */
 
-  const validationSchema = yup.object().shape(FORMVALIDATOR);
+  const validationSchema = yup.object().shape(LOGINVALIDATOR);
   const auth = getAuth();
   const router = useRouter();
   const formik = useFormik({
@@ -61,6 +68,10 @@ const LoginModal: React.FC<{
           .then((userCredential) => {
             const user = userCredential.user;
             setLoggedIn(true);
+            toast({
+              status: "success",
+              description: "Logged in successfully",
+            });
             router.push({
               pathname: "/user/[userID]",
               query: { userID: user.uid },
@@ -69,6 +80,10 @@ const LoginModal: React.FC<{
           .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            toast({
+              status: "error",
+              description: errorMessage,
+            });
           })
           .finally(() => {
             formik.setSubmitting(false);
@@ -125,62 +140,63 @@ const LoginModal: React.FC<{
           >
             {actionDesc}
           </Text>
-
-          <form onSubmit={formik.handleSubmit}>
-            <Stack spacing={4}>
-              <FormControl id="email">
-                <FormLabel htmlFor="email">Email address</FormLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  name="email"
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                  color="#FFF"
-                />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input
-                  id="password"
-                  type="password"
-                  name="password"
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                />
-              </FormControl>
-            </Stack>
-            <Flex
-              alignItems={"center"}
-              justifyContent={"space-around"}
-              gap="20px"
-              pt="2rem"
-            >
-              <Box
-                as={Button}
-                cursor={"pointer"}
-                bgColor={"#4E9060"}
-                borderRadius="18px"
-                p="0.8rem 1rem"
-                type="submit"
-                isLoading={formik.isSubmitting}
-                /* isDisabled={formik.isValid ? false : true} */
-                onClick={() => formik.handleSubmit}
+          <>
+            <form onSubmit={formik.handleSubmit}>
+              <Stack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel htmlFor="email">Email address</FormLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    color="#FFF"
+                  />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input
+                    id="password"
+                    type="password"
+                    name="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                  />
+                </FormControl>
+              </Stack>
+              <Flex
+                alignItems={"center"}
+                justifyContent={"space-around"}
+                gap="20px"
+                pt="2rem"
               >
-                {yesText}
-              </Box>
+                <Box
+                  as={Button}
+                  cursor={"pointer"}
+                  bgColor={"#4E9060"}
+                  borderRadius="18px"
+                  p="0.8rem 1rem"
+                  type="submit"
+                  isLoading={formik.isSubmitting}
+                  isDisabled={formik.isValid ? false : true}
+                  onClick={() => formik.handleSubmit}
+                >
+                  {yesText}
+                </Box>
 
-              <Box
-                cursor={"pointer"}
-                bgColor={"red.600"}
-                borderRadius="18px"
-                p="0.8rem 1rem"
-                onClick={onRequestClose}
-              >
-                {noText}
-              </Box>
-            </Flex>
-          </form>
+                <Box
+                  cursor={"pointer"}
+                  bgColor={"red.600"}
+                  borderRadius="18px"
+                  p="0.8rem 1rem"
+                  onClick={onRequestClose}
+                >
+                  {noText}
+                </Box>
+              </Flex>
+            </form>
+          </>
         </Box>
       </ModalCmp>
     </>

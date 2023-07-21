ce4d12d49d5a70de138722baeb7186bd6ed7f91f
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-07-21 23:25:34 +0100

    remove unused imports / variable

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
-import {
-  Box,
-  Flex,
-  HStack,
-  IconButton,
-  Menu,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-  Image,
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { Box, Flex, HStack, useDisclosure, Image } from "@chakra-ui/react";
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <Box
-        bg="#aec3b0"
-        px={4}
-      >
+      <Box bg="#aec3b0" px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={""}>
-          {/* <IconButton
-            size={"md"}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={"Open Menu"}
-            display={{ md: "none" }}
-            onClick={isOpen ? onClose : onOpen}
-          /> */}
           <HStack spacing={8} alignItems={"center"}>
             <HStack
               as={"nav"}
@@ -48,26 +24,7 @@ const Navbar = () => {
               </Link>
             </HStack>
           </HStack>
-          {/* <Flex alignItems={"center"}>
-            <Menu>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex> */}
         </Flex>
-
-        {/* {isOpen ? (
-          <Box pb={4} display={{ md: "none" }}>
-            <Stack as={"nav"} spacing={4}>
-              <Link href="/">
-                <Image src="/Logo.png" alt="logo" />
-              </Link>
-            </Stack>
-          </Box>
-        ) : null} */}
       </Box>
     </>
   );
